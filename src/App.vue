9d2b69c52c62ee5018cdf123045e843097d89f64
Ogsp1rit <98807417+Ogsp1rit@users.noreply.github.com>
2022-11-12 04:14:08 -0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <v-typical
+    class="blink"
+    :steps="['🕑s', 250, '🕑so', 250, '🕑soo', 250, '🕑soon', 250,, '🕑soon.', 250, '🕑soon..', 250, '🕑soon...', 250, '🕑soon...', 250, ]"
+    :loop="Infinity"
+    :wrapper="'h2'"
+  ></v-typical>
+</template>
+
+<script lang="ts">
+import VTypical from 'vue-typical';
+
+export default {
+  components: {
+    VTypical,
+  },
+};
+</script>
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.typicalWrapper::after {
+  content: "|";
+  animation: blink 1s infinite step-start;
+}
+
+@keyframes blink {
+  50% { opacity: 0; }
+}
+
+a {
+  text-align: center;
+  font-size: x-large;
+  transition: 2s;
+}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 5rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+  .text {
+    text-align: center;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "./assets/main.css";
+import VTypical from 'vue-typical';
+
+createApp(App).mount("#app");
